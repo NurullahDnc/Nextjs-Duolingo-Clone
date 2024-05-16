f13ee03937a3f18121a706a6c9925a05096c80b8
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2024-05-16 21:08:22 +0300

    The project has been initialized, the 'shadcnUi' library has been added. Button components and the layout structure have been created.

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,8 @@
+
+export const Header =()=>{
+     return(
+        <header className="h-20 w-full border-slate-200 px-4 border-b-2 ">
+            Header
+        </header>
+     )
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+ 
+export default function Home() {
+  return (
+    <div>
+      <p>merhaba</p>
+      <Button > click me </Button>
+    </div>
+  );
+}
