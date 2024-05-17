d4eaaabd8cd1f63e7d4e600bd26ba63d5b1b3fc1
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2024-05-18 00:48:24 +0300

    footer completed, flags added

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,8 +1,46 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
-export const Footer =()=>{
-    return(
-        <div className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2 ">
-            footer
-        </div>
-    )
-}
+export const Footer = () => {
+  interface Language {
+    name: string;
+    image: string;
+  }
+
+  const data: Language[] = [
+    {
+      name: "Turkey",
+      image: "/tr.svg",
+    },
+    {
+      name: "Italıan",
+      image: "/it.svg",
+    },
+    {
+      name: "JAPANESE",
+      image: "/jp.svg",
+    },
+    {
+      name: "FRENCH",
+      image: "/fr.svg",
+    },
+  ];
+  return (
+    <div className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2 ">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full ">
+        {data.map((item: any) => (
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src={item.image}
+              alt={item.name}
+              width={40}
+              height={32}
+              className="mr-4 rounded-md"
+            />
+            {item.name}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
