b74dabb9afb97a7392fc1fa81adbac3d9501b025
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2024-05-18 19:04:53 +0300

    drizzle and neon installed, database connection established

---
+++ components/feed-wrapper.tsx
@@ -0,0 +1,14 @@
+
+interface FeedWrapperProps {
+    children: React.ReactNode
+}
+
+const FeedWrapper = ({children}: FeedWrapperProps) => {
+    return ( 
+        <div className="flex-1 relative top-0 pb-10  ">
+            {children}
+        </div>
+     );
+}
+ 
+export default FeedWrapper;
