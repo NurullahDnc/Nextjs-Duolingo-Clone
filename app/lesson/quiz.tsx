93d2fc9f28ca857b26e109cea8acea24b23f5e85
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2024-05-24 00:30:37 +0300

    Added cards and sounds to the lesson page, used the react-use library.

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -2,6 +2,9 @@
 import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import Header from "./header";
+import QuestionBubble from "./question-bubble";
+import Challenge from "./challenge";
+import Footer from "./footer";
 
 interface QuizProps {
   initialLessonId: number;
@@ -23,6 +26,28 @@ const Quiz = ({
 }: QuizProps) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncomplatedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncomplatedIndex === -1 ? 0 : uncomplatedIndex;
+  });
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
+  const onSelect = (id: number) => {
+    if (status !== "none") return;
+    setSelectedOption(id);
+  };
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
 
   return (
     <>
@@ -31,7 +56,35 @@ const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
-      
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center ">
+          {/* justify-center eklendi */}
+          <div className="lg:min-h-[350px] justify-center lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12   ">
+            <h1 className="text-lg lg:text-3xl  text-center lg:text-start font-bold text-neutral-700 ">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer
+        disabled={!selectedOption}
+        status={status}
+        onCheck={() => {}}
+        lessonId={true}
+      />
     </>
   );
 };
